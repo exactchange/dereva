7edc2eb69cac3f11397bd2a27f7c0b8463871795
hello@bennyschmidt.com <hello@bennyschmidt.com>
2021-12-05 11:28:40 -0800

    update peers

--- src/peers.js
+++ src/peers.js
@@ -1,4 +1,3 @@
 module.exports = {
-  "ff0b0fe7-1d46-764a-2b82-9751147cbdd8": "https://www.exactchange.network/ember",
-  "bee79f75-5865-0f4d-d7af-e00091504e19": "https://caymans.herokuapp.com"
+  "ff0b0fe7-1d46-764a-2b82-9751147cbdd8": "https://www.exactchange.network/ember"
 }
