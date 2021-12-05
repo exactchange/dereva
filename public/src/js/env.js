de426fd628a350d0fd5f4b5e4fa9a481e2e5f412
hello@bennyschmidt.com <hello@bennyschmidt.com>
2021-12-05 11:09:23 -0800

    update embercoin

--- public/src/js/env.js
+++ public/src/js/env.js
@@ -1,6 +1,1 @@
-window.env = {
-  PEER_LIST: {
-    "ff0b0fe7-1d46-764a-2b82-9751147cbdd8": "//exactchange.herokuapp.com/ember",
-    "bee79f75-5865-0f4d-d7af-e00091504e19": "//caymans.herokuapp.com"
-  }
-};
+window.env = {};
---
+++ src/peers.js
@@ -0,0 +1,4 @@
+module.exports = {
+  "ff0b0fe7-1d46-764a-2b82-9751147cbdd8": "https://exactchange.herokuapp.com/ember",
+  "bee79f75-5865-0f4d-d7af-e00091504e19": "https://caymans.herokuapp.com"
+}
